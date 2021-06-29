c2fdcd52b5516fa612659dbc3a63e6cf44a1cf7f
Asyuiwish <289155951@qq.com>
2021-06-29 16:07:17 +0800

    1

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'C:/Users/yuna/Desktop/yuna/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
