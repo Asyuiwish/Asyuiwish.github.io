4b19513f36fa1b6058b62c42ad529e0e4e27cd93
Asyuiwish <289155951@qq.com>
2021-07-01 18:27:11 +0800

    添加网页图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
   title: 'Yui的学习笔记',  // 设置网站标题
   description : 'Yui的学习笔记',
+  head: [
+    [
+        'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+        { rel: 'icon', href: 'lemon.png' }
+    ]
+  ],
   base : '/',
   themeConfig : {
     search: false,
